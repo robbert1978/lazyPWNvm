6bc864d2ee168cb097f78be5afdaa4eb67d04aab
robbert1978 <ngovinhhuyhl@gmail.com>
2023-09-10 14:34:15 +0700

    update some tools

--- lazy_pwn.sh
+++ lazy_pwn.sh
@@ -24,6 +24,7 @@ sudo apt-get install -y python-is-python3
 
 cd ~/ && \
 mkdir -p tools && \
+sudo ln -s ~/tools /tools && \
 cd tools
 
 # Install stable pwndbg
@@ -59,6 +60,21 @@ git clone https://github.com/niklasb/libc-database.git
 
 # Script for adding libc's source code to gdb
 wget -O ~/tools/add_glibc_src.py https://gist.githubusercontent.com/robbert1978/84a345665c23171028d474cc699c236a/raw/1415cfed04c5f428bb65378243c81c2c3f56a690/add_glibc_src.py
+
+
+
+# initramfs tools
+wget -O ~/tools/compress.sh https://gist.github.com/robbert1978/ae191c169b81fb99038986b7636d18f1/raw/b2846f340146cb4abf789fd562a4ac968e09d0a5/compress.shm && \
+chmod +x ~/tools/compress.sh
+
+wget -O ~/tools/decompress.sh https://gist.github.com/robbert1978/ae191c169b81fb99038986b7636d18f1/raw/b2846f340146cb4abf789fd562a4ac968e09d0a5/decompress.sh && \
+chmod +x ~/tools/decompress.sh
+
+
+# rp++
+wget -O /tmp/rp-lin-clang.zip https://github.com/0vercl0k/rp/releases/download/v2.1.2/rp-lin-clang.zip && \
+unzip /tmp/rp-lin-clang.zip &&
+chmod +x rp-lin
 
 # Exit "tools" dir
 cd ../
--- lazy_zsh.sh
+++ lazy_zsh.sh
@@ -19,9 +19,14 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 #Custom zsh
 cd ~/
 rm -rf ~/.zshrc && \
-wget https://raw.githubusercontent.com/owl4ce/dotfiles/c5c33b46ef90131fff88c2b9b954cab27e0b0972/EXTRA_JOYFUL/.zshrc
+wget https://gist.github.com/robbert1978/912225ea9033e5cdc27a936f615220ff/raw/169dc4c5242cef58f943d9a974ab1c8c46f9d93a/.zshrc
+
+rm -rf ~/.oh-my-zsh/customs/theme/robbert.zsh-theme && \
+wget https://gist.github.com/robbert1978/912225ea9033e5cdc27a936f615220ff/raw/169dc4c5242cef58f943d9a974ab1c8c46f9d93a/robbert.zsh-theme
 
 #Install zsh plugins
 git clone --depth 1 https://github.com/zdharma-continuum/fast-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/fast-syntax-highlighting
 git clone --depth 1 https://github.com/zsh-users/zsh-autosuggestions.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone --depth 1 https://github.com/zsh-users/zsh-completions.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-completions
+
+
